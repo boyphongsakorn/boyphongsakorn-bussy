749f83d8bda2d5f05cf9721ad1e2cd34f8899f09
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-07-02 13:45:44 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Styles,Card,Row,Col,Icon } from 'sveltestrap';
   let name = ''
-  if(window.location.hostname == 'pwisetthon' || window.location.hostname == 'pwisetthon.com'){
+  if(window.location.hostname.includes('pwisetthon')){
     name = 'พงศกร วิเศษธร (บอย)';
   }else{
     name = 'พนักงานบริษัท ทีมควอดบี จำกัด';
@@ -19,7 +19,7 @@
   <Row>
     <Col sm="12" md={{ size: 4, offset: 4 }}>
       <Card class="mb-1" body><center><h2>ตารางงานของ {name}</h2></center></Card>
-      <Card body><p class="mb-0"><Icon name="info-square-fill" /> ยังไม่มีตารางงานของ {name}</p></Card>
+      <!--Card body><p class="mb-0"><Icon name="info-square-fill" /> ยังไม่มีตารางงานของ {name}</p></Card-->
       <Card body>
         <p><Icon name="calendar-event" /> วันที่ 1 สิงหาคม 2565</p>
         <p><Icon name="shield-fill" /> นนทุบรี</p>
