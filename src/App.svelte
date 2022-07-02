381c9f58c6e8a1c1fe57ff9f085b184a29d2e4bd
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-07-02 09:41:56 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -20,6 +20,11 @@
     <Col sm="12" md={{ size: 4, offset: 4 }}>
       <Card class="mb-1" body><center><h2>ตารางงานของ {name}</h2></center></Card>
       <Card body><p class="mb-0"><Icon name="info-square-fill" /> ยังไม่มีตารางงานของ {name}</p></Card>
+      <Card body>
+        <p><Icon name="calendar-event" /> วันที่ 1 สิงหาคม 2565</p>
+        <p><Icon name="shield-fill" /> นนทุบรี</p>
+        <p><Icon name="clock-fill" /> ทั้งวัน</p>
+      </Card>
     </Col>
   </Row>
 </main>
