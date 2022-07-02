15986f21964bf9fa8720fde9b8f321f8b76e8653
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-07-02 09:21:09 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
-  //import logo from './assets/svelte.png'
-  //import Counter from './lib/Counter.svelte'
+  import { Styles,Card,Row,Col,Icon } from 'sveltestrap';
+  let name = ''
+  if(window.location.hostname == 'pwisetthon' || window.location.hostname == 'pwisetthon.com'){
+    name = 'พงศกร วิเศษธร (บอย)';
+  }else{
+    name = 'พนักงานบริษัท ทีมควอดบี จำกัด';
+  }
+  console.log(window.location.hostname)
 </script>
 
+<Styles />
+
 <main>
-  testtest
+  <Row>
+    <Col sm="12" md={{ size: 4, offset: 4 }}>
+      <Card class="mb-1" body><center><h2>ตารางงานของ {name}</h2></center></Card>
+      <Card body><p class="mb-0"><Icon name="info-square-fill" /> ยังไม่มีตารางงานของ {name}</p></Card>
+    </Col>
+  </Row>
 </main>
 
 <style>
