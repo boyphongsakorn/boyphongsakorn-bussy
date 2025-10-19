f2a41fef2b8cce002c026e78153f6b6701f2b682
Phongsakorn Wisetthon <3013842+boyphongsakorn@users.noreply.github.com>
2025-10-19 12:27:22 +0000

    Update image URL for Phuket location in event list

--- src/App.svelte
+++ src/App.svelte
@@ -224,7 +224,7 @@
         levents[i][6] = 'https://img.gs/fhcphvsghs/120x120,crop/https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Seal_of_Bangkok_Metro_Authority.png/2048px-Seal_of_Bangkok_Metro_Authority.png'
       }
       if(levents[i][2].trim() == 'ภูเก็ต'){
-        levents[i][6] = 'https://www.phuket.go.th/webpk/images/introduce/logo-phuket2565.jpg'
+        levents[i][6] = 'https://anywhere.pwisetthon.com/https://www.phuket.go.th/webpk/images/introduce/logo-phuket2565.jpg'
       }
       if(levents[i][2].trim() == 'ขอนแก่น'){
         levents[i][6] = 'https://img.gs/fhcphvsghs/120x120,crop/https://khonkaen.m-culture.go.th/web-upload/1005x9680e19a89465bf0531f017d8ef94780/tinymce/94-bfc6edecc2a4da646bd0824086ba8dea/%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B9%8C%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%B3%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94/khonkaenLogo.png'
